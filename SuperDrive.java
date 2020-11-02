178b9a4bdca9f91a7eb0c743309ee4e86fa1c4bb
Oliver Santiago Kermani <osantiagokermani20@stuy.edu>
2020-11-02 15:57:47 -0500

    Tests

--- SuperDrive.java
+++ SuperDrive.java
@@ -5,5 +5,8 @@ public class SuperDrive{
       System.out.println(one.size());
       one.add("Hello");
       System.out.println(Arrays.toString(one.printarray()));
+      System.out.println(one.get(9));
+      System.out.println(one.set(3,"Third"));
+      System.out.println(Arrays.toString(one.printarray()));
   }
 }
