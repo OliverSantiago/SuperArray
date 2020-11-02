7022ac7a092c4bd331b318d2476f1565619f76f4
Oliver Santiago Kermani <osantiagokermani20@stuy.edu>
2020-11-02 15:53:58 -0500

    Tests

--- SuperDrive.java
+++ SuperDrive.java
@@ -1,6 +1,9 @@
+import java.util.Arrays;
 public class SuperDrive{
     public static void main(String[]args){
       SuperArray one = new SuperArray();
       System.out.println(one.size());
+      one.add("Hello");
+      System.out.println(Arrays.toString(one.printarray()));
   }
 }
