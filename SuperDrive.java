5ccad64cd465b9dfc85ee5e736f723a7b39c6c22
Oliver Santiago Kermani <osantiagokermani20@stuy.edu>
2020-11-03 14:29:59 -0500

    Fixed misinterpretatoin of previous instructions

--- SuperDrive.java
+++ SuperDrive.java
@@ -1,24 +1,43 @@
 import java.util.Arrays;
 public class SuperDrive{
     public static void main(String[]args){
-      SuperArray one = new SuperArray();
-      SuperArray two = new SuperArray();
-      two.add("Second Array");
+      //SuperArray one = new SuperArray();
+      //SuperArray two = new SuperArray();
+      //two.add("Second Array");
       //System.out.println(one.size());
-      System.out.println(Arrays.toString(one.printarray()));
-      one.add("Hello");
+      //System.out.println(Arrays.toString(one.printarray()));
+      //one.add("Hello");
       //System.out.println(Arrays.toString(one.printarray()));
       //System.out.println(one.get(9));
       //System.out.println(one.set(3,"Third"));
       //System.out.println(Arrays.toString(one.printarray()));
       //one.add("Hello");
       //System.out.println(Arrays.toString(one.printarray()));
-      System.out.println(Arrays.toString(one.printarray()));
-      System.out.println(one.add("Hello"));
-      System.out.println(Arrays.toString(one.printarray()));
-      System.out.println(one.isEmpty());
-      one.clear();
-      System.out.println(Arrays.toString(one.printarray()));
-      System.out.println(two.toString());
+      //System.out.println(Arrays.toString(one.printarray()));
+      //System.out.println(one.add("Hello"));
+      //System.out.println(Arrays.toString(one.printarray()));
+      //System.out.println(one.isEmpty());
+      //one.clear();
+      //System.out.println(Arrays.toString(one.printarray()));
+      //System.out.println(two.toString());
+      SuperArray test = new SuperArray();
+      System.out.println(test.size());
+      System.out.println(test.add("First String"));
+      System.out.println(test.add("Second String"));
+      System.out.println(test.size());
+      System.out.println(test.get(1));
+      System.out.println(test.set(1,"New Second String"));
+      System.out.println(test.add("Third String"));
+      System.out.println(test.add("Fourth String"));
+      System.out.println(test.add("Fifth String"));
+      System.out.println(test.add("Sixth String"));
+      System.out.println(test.add("Seventh String"));
+      System.out.println(test.add("Eighth String"));
+      System.out.println(test.add("Ninth String"));
+      System.out.println(test.add("Tenth String"));
+      System.out.println(Arrays.toString(test.printarray()));
+      System.out.println(test.add("Eleventh String"));
+      System.out.println(Arrays.toString(test.printarray()));
+
   }
 }
