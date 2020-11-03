7f76426130cd3abdff9eedebfb269651e3301add
Oliver Santiago Kermani <osantiagokermani20@stuy.edu>
2020-11-03 13:53:33 -0500

    errors and method

--- SuperDrive.java
+++ SuperDrive.java
@@ -2,13 +2,23 @@ import java.util.Arrays;
 public class SuperDrive{
     public static void main(String[]args){
       SuperArray one = new SuperArray();
-      System.out.println(one.size());
-      one.add("Hello");
-      System.out.println(Arrays.toString(one.printarray()));
-      System.out.println(one.get(9));
-      System.out.println(one.set(3,"Third"));
+      SuperArray two = new SuperArray();
+      two.add("Second Array");
+      //System.out.println(one.size());
       System.out.println(Arrays.toString(one.printarray()));
       one.add("Hello");
+      //System.out.println(Arrays.toString(one.printarray()));
+      //System.out.println(one.get(9));
+      //System.out.println(one.set(3,"Third"));
+      //System.out.println(Arrays.toString(one.printarray()));
+      //one.add("Hello");
+      //System.out.println(Arrays.toString(one.printarray()));
       System.out.println(Arrays.toString(one.printarray()));
+      System.out.println(one.add("Hello"));
+      System.out.println(Arrays.toString(one.printarray()));
+      System.out.println(one.isEmpty());
+      one.clear();
+      System.out.println(Arrays.toString(one.printarray()));
+      System.out.println(two.toString());
   }
 }
