bd5a07591c31eca5af7bc974cc646bd1232e4e28
Oliver Santiago Kermani <osantiagokermani20@stuy.edu>
2020-11-08 20:15:00 -0500

    Demo

--- Demo.java
+++ Demo.java
@@ -9,6 +9,31 @@ public class Demo{
     }
   }
 
+  public static SuperArray zip(SuperArray a, SuperArray b){
+    int maxi = a.size()+b.size();
+    SuperArray answer = new SuperArray();
+    boolean A = true;
+    int indexA = 0;
+    int indexB = 0;
+    for (int i = 0; i < maxi; i++){
+      if (A){
+        if(indexA<a.size()){
+          answer.add(a.get(indexA));
+          indexA++;
+        }
+        A = false;
+      }else{
+        if (indexB<b.size()){
+          answer.add(b.get(indexB));
+          indexB++;
+        }
+        A = true;
+      }
+    }
+    return answer;
+
+  }
+
   public static SuperArray findOverlap(SuperArray a, SuperArray b){
     SuperArray answer = new SuperArray();
     for (int i = 0; i < a.size(); i++){
@@ -45,5 +70,23 @@ public class Demo{
 
     System.out.println(words);
     removeDuplicates(words);
-    System.out.println(words);  }
+    System.out.println(words);
+
+
+    SuperArray one = new SuperArray();
+    one.add("a");
+    one.add("b");
+    one.add("c");
+    one.add("d");
+    one.add("e");
+    one.add("f");
+    SuperArray two = new SuperArray();
+    two.add("0");
+    two.add("1");
+    two.add("2");
+    two.add("3");
+
+    System.out.println(zip(one,two).toString());
+
+  }
 }
